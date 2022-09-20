5647d131065febb8952c0bfef434afe5f48ea15e
Daniele Marinazzo <daniele.marinazzo@gmail.com>
2022-09-20 14:20:27 +0200

    separated loops for timing

--- test_timevarying.m
+++ test_timevarying.m
@@ -1,50 +1,70 @@
 % this script tests several connectivity measures in a time-resolved way,
 % i.e. computing phase consistency across trials per each time point.
 % The simulated data represent coupling starting at 1/3 of the trial,
-% either with instantaneous coupling, or with lagged one. 
+% either with instantaneous coupling, or with lagged one.
 
 clear
 load test_PLI_timevarying
 
 % choose here "delay" or "nodelay". datah is already filtered and hilbert transformed
- datah=datah_simulated_delay; %lagged influence, also corrected measures should change
+datah=datah_simulated_delay; %lagged influence, also corrected measures should change
 % datah=datah_simulated_nodelay; %zero lag influence, only noncorrected measures should change
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-[nc, ns, nt]=size(datah);
+[nc, ns, nt]=size(datah); %channels per trials per timepoints
 nA=2;nB=2;
 iA = repmat(1:nA, 1, nB)';
 iB = reshape(repmat(1:nB, nA, 1), [], 1);
 
-ndat=datah ./ abs(datah);
-
+% Brainstorm-style implementation - initialize the "R" structures
 R_PLV=complex(zeros(nt,nA*nB),0);R_ciPLV=R_PLV;R_WPLI=R_PLV;
 R_dbWPLI_num=R_PLV;R_dbWPLI_den=R_PLV;R_dbWPLI_sqd=R_PLV;
-% Brainstorm-style implementation
+
 tic
 for itrials=1:ns
     HA=squeeze(datah(:,itrials,:));HB=squeeze(datah(:,itrials,:));
     phaseA = HA(iA,:) ./ abs(HA(iA,:));
     phaseB = HB(iB,:) ./ abs(HB(iB,:));
     R_PLV=R_PLV+(phaseA .* conj(phaseB))';
+end
+PLV=abs(R_PLV(:,2)/ns);
+t=toc;disp(['PLV, ' num2str(t) ' seconds']);
+tic
+for itrials=1:ns
+    HA=squeeze(datah(:,itrials,:));HB=squeeze(datah(:,itrials,:));
+    phaseA = HA(iA,:) ./ abs(HA(iA,:));
+    phaseB = HB(iB,:) ./ abs(HB(iB,:));
     R_ciPLV=R_ciPLV+(imag((phaseA .* conj(phaseB))'))./(real((phaseA .* conj(phaseB))')/nt).*conj((real(phaseA .* conj(phaseB))')/nt);
-    cdd = phaseA .* conj(phaseB);
-    cdi = imag(cdd);
+end
+ciPLV=abs(R_ciPLV(:,2)/ns);
+t=toc;disp(['ciPLV, ' num2str(t) ' seconds']);
+tic
+for itrials=1:ns
+    HA=squeeze(datah(:,itrials,:));HB=squeeze(datah(:,itrials,:));
+    phaseA = HA(iA,:) ./ abs(HA(iA,:));
+    phaseB = HB(iB,:) ./ abs(HB(iB,:));
+    cdi = imag(phaseA .* conj(phaseB));
     R_WPLI=R_WPLI+(abs(cdi).*sign(cdi))'./abs(cdi)';
+end
+wPLI=abs(R_WPLI/ns);
+t=toc;disp(['wPLI, ' num2str(t) ' seconds']);
+tic
+for itrials=1:ns
+    HA=squeeze(datah(:,itrials,:));HB=squeeze(datah(:,itrials,:));
+    phaseA = HA(iA,:) ./ abs(HA(iA,:));
+    phaseB = HB(iB,:) ./ abs(HB(iB,:));
+    cdi = imag(phaseA .* conj(phaseB));
     R_dbWPLI_num=R_dbWPLI_num+(abs(cdi).*sign(cdi))';
     R_dbWPLI_sqd=R_dbWPLI_sqd-(((abs(cdi).*sign(cdi))').^2)/nt;
     R_dbWPLI_den=R_dbWPLI_den+abs(cdi)';
 end
-toc
-
-% here just abs and divide by the number of trials, except for the debiased
-% wPLI. We should find a solution for this latter part for implementation in
-% Brainstorm 
-
-PLV=abs(R_PLV(:,2)/ns);
-ciPLV=abs(R_ciPLV(:,2)/ns);
-wPLI=abs(R_WPLI/ns);
+% the syntax below is not what currently implemented in Brainstorm, which
+% does just division by number of trials and take the abs, as for the
+% previous measures
 wPLI_db=sqrt((R_dbWPLI_num.^2-R_dbWPLI_sqd)./((R_dbWPLI_den.^2-R_dbWPLI_sqd)));
+t=toc;disp(['debiased wPLI, ' num2str(t) ' seconds']);
+
+
 figure
 plot(PLV)
 hold on
